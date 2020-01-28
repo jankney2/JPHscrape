a6a9964c3d885e213cdde306689a6fc237dbdbf9
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-01-28 16:20:11 -0700

    framework set

---
+++ index.js
@@ -0,0 +1,55 @@
+const puppeteer=require('puppeteer')
+
+
+
+
+
+
+
+//helpful stuff
+//first input
+
+
+function scrapeJPH(){
+
+//first input
+    document.getElementById('CompanyId')
+//type 108022
+
+//tab
+//type username
+//tab
+//type pass
+
+//login button
+document.getElementsByClassName('pcty-button pcty-size-medium pcty-button-full-width pcty-button-full-width-mobile pcty-size-responsive submit-button login-button')[0]
+
+//click
+
+//evaluate text for challenge question
+
+document.getElementById('ChallengeQuestionText')
+
+
+//create answer
+
+//type answer
+
+//click login button
+
+document.getElementById('pcty-button-submit')
+
+//wait for selector
+document.querySelector('div[class=header-nav]')
+
+//navigate to reports page
+
+//get these, click the dynamic reports one
+document.querySelectorAll('a[class=pcty-target]')
+
+
+//download report
+
+
+//use file server to access file, save to database 
+}
